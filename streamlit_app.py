af2eef3b57388e35f6a9dfc085b170981733c53a
Toshikuni Fuji <77420797+ToshikuniFuji@users.noreply.github.com>
2023-07-22 15:11:02 +0900

    Update streamlit_app.py Day11

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ st.header('st.multiselect')
 
 options = st.multiselect(
      'What are your favorite colors?',
-     ['Green', 'Yellow', 'Red', 'Blue'])
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
 
 st.write('Your selected:', options)
